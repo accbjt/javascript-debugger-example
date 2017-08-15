925d97cfbace6d6eb0c5b6aee58e82eb0f74c26f
Bill Tran <bill@billjohntran.com>
2017-08-14 18:10:24 -0700

    added files

---
+++ example.js
@@ -0,0 +1,18 @@
+var cat = new Object()
+
+cat.name = 'Cat';
+cat.says = "meow"
+
+var dog = {
+  name: "Dog",
+  says: "woof woof",
+}
+
+var items = new Array();
+
+var foo = [1, 2];
+var bar = foo;
+
+bar[0] = 9;
+
+console.log(foo[0], bar[0]);
---
+++ jquery-project/index.js
@@ -0,0 +1,35 @@
+// Declare the variables we intend to use later.
+var cats;
+var noms;
+
+// Update the treatDispenser function to loop over every animal and check if its name begins with "M".
+
+// If it does, add a message to the results array saying 'No treat for {AnimalName}.'.
+// Otherwise, add a message that says '{AnimalName} loved their treat!'
+// In both cases, replace {AnimalName} with the actual name of the animal.
+
+// Next, create the function we'll use. This is what you'll modify to make the
+// code work.
+
+function treatDispenser (animals) {
+  // This array will hold all of the messages we'll return from the function.
+  var results = [];
+
+  // Hint: Put your super awesome loop here. :)
+
+  return results;
+}
+
+cats = [
+  'Jayne',
+  'Kaylee',
+  'Malcolm',
+  'Monty',
+  'River',
+  'Simon',
+  'Zoë'
+];
+
+noms = treatDispenser(cats);
+
+console.log(noms);
